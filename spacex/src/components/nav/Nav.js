17e30ba662e86aa2579b0f6738877b140d1e0348
Zimbo333 <thanawat..kemwat@gmail.com>
2021-02-18 01:09:38 +0700

    Nav can route now

--- spacex/src/components/nav/Nav.js
+++ spacex/src/components/nav/Nav.js
@@ -1,21 +1,28 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import "./Nav.css"
 
 function Nav() {
     return (
-      <div className="container mx-auto p-3 bg-black text-white w-full">
+      <div>
+        <div className="container mx-auto p-3 bg-black text-white w-full">
           <div className="grid grid-cols-12 items-center">
             <img className="md:col-span-3 col-span-3 ml-4 cursor-pointer" src="/rocket.png"></img>
-            <div className="md:col-span-9 col-span-9 flex flex-row-reverse pr-16">
-              <button className="p-4 hover:border-2 hover:border-white hover:text-red-400 hover:border-rounded">HOME</button>
-              <button className="p-4 hover:border-2 hover:border-white hover:text-red-400 hover:border-rounded">ROCKET</button>
-              <button className="p-4 hover:border-2 hover:border-white hover:text-red-400 hover:border-rounded">LAUNCHES</button>
+            <div className="md:col-span-9 col-span-9 flex flex-row justify-end pr-16">
+              <Link to="/"><button className="p-4 hover:border-2 hover:border-white hover:text-red-400 hover:border-rounded">HOME</button></Link>
+              <Link to="/Rocket"><button className="p-4 hover:border-2 hover:border-white hover:text-red-400 hover:border-rounded">ROCKET</button></Link>
+              <Link to="/Launch"><button className="p-4 hover:border-2 hover:border-white hover:text-red-400 hover:border-rounded">LAUNCHES</button></Link>
             </div>
+          </div>
         </div>
-        
-        
-        
       </div>
+
     );
   }
 
