3498b84411cb7c94c937a63574b27c6485d86236
Zimbo333 <thanawat..kemwat@gmail.com>
2021-02-19 03:44:06 +0700

    customize RKdetail and init Launch.js

--- spacex/src/Pages/Launch/Launch.js
+++ spacex/src/Pages/Launch/Launch.js
@@ -1,7 +1,20 @@
 function Launch() {
     return (
       <div>
-        <h2>Launch</h2>
+        <div className="flex flex-col items-center justify-center justify-items-center h-64  bg-black">
+          <h1 className="underline text-5xl text-white">Launches List</h1>
+          <div className="pt-16 text-sm">
+              <select className="mx-4" name="" id="" >
+                <option value="">Some Launches</option>
+              </select>
+              <select className="mx-4" name="" id="" >
+                <option value="">10-10-2020</option>
+              </select>
+              <select className="mx-4" name="" id="" >
+                <option value="">true</option>
+              </select>
+          </div>
+        </div>
       </div>
     );
   }
--- spacex/src/Pages/RocketDetail/RocketDetail.js
+++ spacex/src/Pages/RocketDetail/RocketDetail.js
@@ -25,12 +25,20 @@ function RocketDetail(){
 
     return(
         <div>
-            <div className="mx-16 mt-4 text-2xl">
-               <Link to="/Rocket"><span className="flex flex-row  cursor-pointer w-16 "><img src="/left-arrow.svg" className="mx-4"></img><span>Back</span></span></Link>
+            <div>
+                <div className="mx-16 mt-4 text-2xl">
+                <Link to="/Rocket"><span className="flex flex-row  cursor-pointer w-16 "><img src="/left-arrow.svg" className="mx-4"></img><span>Back</span></span></Link>
+                </div>
+                <div className="container flex flex-col items-center justify-center justify-items-center h-screen">
+                    <img className="m-4 h-96 w-96 rounded-full " src={rocketInfo.flickr_images}></img>
+                    <p className="m-4 text-7xl ">{rocketInfo.rocket_name}</p>
+                </div>
             </div>
-            <div className="container flex flex-col items-center justify-center justify-items-center h-screen">
-                <img className="m-4 h-96 w-96 rounded-full " src={rocketInfo.flickr_images}></img>
-                <p className="m-4 text-7xl ">{rocketInfo.rocket_name}</p>
+            <div className="bg-black">
+                <div className="text-white py-32 px-16">
+                    <h1 className="text-yellow-400">{rocketInfo.rocket_name}</h1>
+                    <p>{rocketInfo.description}</p>   
+                </div>    
             </div>
         </div>
         
