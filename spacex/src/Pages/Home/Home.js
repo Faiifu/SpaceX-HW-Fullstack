98c08f4ec474be644d25dd9980c9d4f8be4e9667
Faiifu <Faiifu@github.com>
2021-02-20 06:57:24 +0700

    footer

--- spacex/src/Pages/Home/Home.js
+++ spacex/src/Pages/Home/Home.js
@@ -27,14 +27,18 @@ function Home() {
     );
     return (
       <div className="bg-home min-h-screen min-w-full">
-        <h1 className="text-white text-8xl text-center absolute flex items-center justify-center min-h-screen min-w-full">SPACEX</h1>
+        <div className=" text-center absolute flex flex-col items-center justify-center min-h-screen min-w-full">
+        <h1 className="text-white text-8xl">SPACEX</h1>
+        <p className="text-white pt-5">Elon Musk founder | 2002 founded | 7000 employees<br></br>
+3 vehicles | 3 launch sites | 1 test sites </p>
+        </div>
         <div className="bg-image">
           <img src={'/image-main.png'} className="img-main bg-cover min-w-full"></img>
         </div>
         <div className="section-2">
           <div className="h-screen">
             <h1 className="text-white pt-5 text-2xl text-center">ROCKET</h1>
-              <div className="grid grid-cols-12 gap-5 h-2/4">
+              <div className="grid grid-cols-12 gap-5 h-80">
                 <div className="col-span-2"></div>
                 {/* <img src={rockets[0].flickr_images[0]}> */}
                   <div className="w-full bg-red-200 mt-5 col-span-2">1</div>
@@ -43,22 +47,35 @@ function Home() {
                 <div className="w-full bg-red-200 mt-5 col-span-2">3</div>
                 <div className="w-full bg-red-200 mt-5 col-span-2">4</div>
               </div>
-              <div className="divide-y">
-                <h1 className="more-detail-text text-white text-center mt-5">more detail>></h1>
+              <div className="divide-y divide-gray-400">
+                <h1 className="more-detail-text text-white text-center mt-5 mb-3">more detail>></h1>
                 <div></div>
               </div>
-              <div className="section-3 h-2/5 w-full flex justify-center items-center pt-1">
+              <div className="section-3 h-80 w-full flex justify-center items-center">
                 <div className="w-2/5 text-center">
                   <h1 className="text-white text-3xl">SpaceX</h1>
                   <br></br>
                   <p className="text-white">{info.summary}</p>
                 </div>
-                <div className="w-1/5 bg-yellow-300" >
-                  <img src={'/elonmusk.jpg'} className="img-elon w-full"></img>
+                <div className="w-80">
+                  <img src={'/elonmusk.png'} className="img-elon w-4/5 mx-8"></img>
+                  <p className="text-white text-center">Elon Musk</p>
                 </div>
               </div>
             </div>   
           </div>
+          <footer className="section-3">
+            <div className="w-full h-3/4 mt-5">
+              <div className="flex flex-row justify-center">
+                <img src={'/flickr.png'} className="w-5 pt-5 mx-5"></img>
+                <img src={'/twitter.png'} className="w-5 pt-5 mx-5"></img>
+                <img src={'/instagram.png'} className="w-5 pt-5 mx-5"></img>
+                <img src={'/website.png'} className="w-5 pt-5 mx-5"></img>
+              </div>
+                <h1 className="text-base tect-center flex justify-center mt-4 mb-4">Space Exploration Technologies Corp. is an American aerospace manufacturer and<br></br>
+                space transportation services company headquartered in Hawthorne, California.</h1>
+            </div>
+          </footer>
         </div>
     );
   }
