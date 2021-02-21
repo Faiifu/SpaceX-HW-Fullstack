f4bb0af95dbb8f63d215cbe3b26db069305d38d1
Zimbo333 <thanawat..kemwat@gmail.com>
2021-02-21 23:24:21 +0700

    add filter to launch.js

--- spacex/src/Pages/Launch/Launch.js
+++ spacex/src/Pages/Launch/Launch.js
@@ -10,17 +10,31 @@ import {
 function Launch() {
   const [launches, setLaunches] = useState([]);
   const [count, setCount] = useState(0);
+  const [rocketName, setRocketName] = useState("");
+  const [launchYear, setLaunchYear] = useState("");
+  const [isSuccess, setIsSuccess] = useState("");
   useEffect(() => {
     const fetchLaunches = async () => {
-      const response = await fetch("https://api.spacexdata.com/v3/launches?limit=10")
+      const response = await fetch(`https://api.spacexdata.com/v3/launches?order=DESC&rocket_name=${rocketName}&launch_year=${launchYear}&launch_success=${isSuccess}`)
       const data = await response.json();
       setLaunches(data);  
-      console.log(data)
     };
     fetchLaunches();
   },
-  [],
+  [rocketName,launchYear,isSuccess],
   );
+  const handleRocketNameChange = (e) => {
+    setRocketName(e.target.value)
+  }
+  const handleLaunchYearChange = (e) => {
+    setLaunchYear(e.target.value)
+  }
+  const handleIsSuccessChange = (e) => {
+    setIsSuccess(e.target.value)
+  }
+  const handleCounter = () => {
+    setCount(count + 1)
+  }
   let {path,url} = useRouteMatch()
     return (
       <div>
@@ -28,18 +42,39 @@ function Launch() {
           <h1 className="underline text-5xl text-white">Launches List</h1>
           <div className="pt-16 text-m flex flex-row">
               <div className="">
-                <select className="mx-8 cursor-pointer px-4 py-2" name="" id="" >
-                  <option value="">Some Launches</option>
+                <select className="mx-8 cursor-pointer px-4 py-2" onChange={handleRocketNameChange} name="" id="" >
+                  <option value="">Select Rocket</option>
+                  <option value="Falcon 1">Falcon 1</option>
+                  <option value="Falcon 9">Falcon 9</option>
+                  <option value="Falcon Heavy">Falcon Heavy</option>
+                  <option value="Starship">Starship</option>
                 </select>
               </div>
               <div className="border-solid">
-                <select className="mx-8 cursor-pointer px-4 py-2" name="" id="" >
-                  <option value="">10-10-2020</option>
+                <select className="mx-8 cursor-pointer px-4 py-2"  onChange={handleLaunchYearChange} name="" id="" >
+                  <option value="">Select Year</option>
+                  <option value="2020">2020</option>
+                  <option value="2019">2019</option>
+                  <option value="2018">2018</option>
+                  <option value="2017">2017</option>
+                  <option value="2016">2016</option>
+                  <option value="2015">2015</option>
+                  <option value="2014">2014</option>
+                  <option value="2013">2013</option>
+                  <option value="2012">2012</option>
+                  <option value="2011">2011</option>
+                  <option value="2010">2010</option>
+                  <option value="2009">2009</option>
+                  <option value="2008">2008</option>
+                  <option value="2007">2007</option>
+                  <option value="2006">2006</option>
                 </select>
               </div>
               <div className="border-solid">
-                <select className="mx-8 cursor-pointer px-4 py-2" name="" id="" >
-                  <option value="">true</option>
+                <select className="mx-8 cursor-pointer px-4 py-2" onChange={handleIsSuccessChange} name="" id="" >
+                  <option value="">Select Launch success</option>
+                  <option value="true">Success</option>
+                  <option value="false">Fail</option>
                 </select>
               </div>
           </div>
@@ -52,9 +87,11 @@ function Launch() {
         </div>
         {launches.map((mission)=>(
           <>
-          {mission.flight_number %2 == 0
+          {count %2 == 0
             ?
+            
             <Link to={`${url}/LaunchDetail/${mission.flight_number}`}>
+            {handleCounter}
             <div className="container grid grid-cols-12 bg-yellow-50 justify-center text-center items-center px-32 cursor-pointer hover:bg-yellow-100">
               <p className="col-span-3 mx-2 text-s py-4">{mission.launch_year}</p>
               <p className="col-span-3 mx-2 text-s py-4">{mission.mission_name}</p>
@@ -64,6 +101,7 @@ function Launch() {
             </Link>
             :
             <Link to={`${url}/LaunchDetail/${mission.flight_number}`}>
+              {handleCounter}
             <div className="container grid grid-cols-12 bg-gray-200 justify-center text-center items-center px-32 cursor-pointer hover:bg-gray-300">
               <p className="col-span-3 mx-2 text-s py-4">{mission.launch_year}</p>
               <p className="col-span-3 mx-2 text-s py-4">{mission.mission_name}</p>
