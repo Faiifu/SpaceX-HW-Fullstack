10e41f4dd9435839a2263c0715994d5b933c7dae
Faiifu <Faiifu@github.com>
2021-02-22 00:45:00 +0700

    logo

--- spacex/src/components/nav/Nav.js
+++ spacex/src/components/nav/Nav.js
@@ -13,7 +13,7 @@ function Nav() {
       <div>
         <div id="bg-home" className="container mx-auto min-w-full text-white w-full">
           <div className="grid grid-cols-12 items-center">
-            <img className="md:col-span-3 col-span-3 ml-4 cursor-pointer" src="/rocket.png"></img>
+            <img className="md:col-span-3 col-span-3 ml-12 cursor-pointer w-2/5" src="/SpaceX_Logo.png"></img>
             <div className="md:col-span-9 col-span-9 flex flex-row justify-end pr-16">
               <Link to="/"><button className="p-4 hover:border-2 hover:border-white hover:text-red-400 hover:border-rounded">HOME</button></Link>
               <Link to="/Rocket"><button className="p-4 hover:border-2 hover:border-white hover:text-red-400 hover:border-rounded">ROCKET</button></Link>
