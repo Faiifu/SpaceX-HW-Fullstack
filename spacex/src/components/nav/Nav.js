36cb1f4750bafe2ca5fc4f1ab89a2e763493b94d
Faiifu <Faiifu@github.com>
2021-02-22 02:31:55 +0700

    nav success

--- spacex/src/components/nav/Nav.js
+++ spacex/src/components/nav/Nav.js
@@ -13,8 +13,8 @@ function Nav() {
       <div>
         <div id="bg-home" className="text-white">
           <div className="grid grid-cols-12 items-center ">
-            <img className="md:col-span-3 col-span-3 ml-12 cursor-pointer w-2/5" src="/SpaceX_Logo.png"></img>
-              <div className="md:col-span-9 col-span-9 flex justify-end pr-10">
+            <NavLink to="/"><img className="md:col-span-3 col-span-3 ml-12 cursor-pointer" src="/SpaceX_Logo.png"></img></NavLink>
+              <div className="md:col-span-11 col-span-11 flex justify-end pr-11">
                 <NavLink exact to="/" activeStyle={{fontWeight: "bold", color: "#FFC300"}}>
                   <p className="p-4 hover:text-yellow-400">Home</p>
                 </NavLink>
